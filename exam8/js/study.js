16725ff5d205ab4115542722813fdaab2ea9eabe
dajung <vlueviolet@gmail.com>
2017-04-05 19:24:40 +0900

    커밋

--- exam8/js/study.js
+++ exam8/js/study.js
@@ -12,22 +12,32 @@
             this.obj = $('.swiper-wrapper .swiper-slide');
         },
         setArray : function () {
-            this._length = this.obj.length;
             this.arr=[];
             for(var i=0, max=this.obj.length ; i < max ; i++){
                 this.arr.push(this.obj.eq(i));
             }
         },
+        setOption : function () {
+            this.curNum;
+            this.prevNum;
+        },
         bindEvents : function () {
             this.btn.on('click', $.proxy(this.randomView,this));
         },
-        setOption : function () {
-            
-        },
-        makeRandomNum : function () {
-            var curNum = Math.floor(Math.random()*this._length);
-            var prevNum=curNum;
-            return curNum;
+        randomView : function () {
+            this._length = this.arr.length;
+            this.curNum = Math.floor(Math.random()*this._length);
+            console.log(this._length);
+
+            for(var i=0, max=this.arr.length ; i < max ; i++){
+                this.arr[i].hide();
+            }
+            this.arr[this.curNum].show();
+            this.arr.splice(this.curNum,1);
+            this.prevNum=this.curNum;
+
+            console.log(this.curNum);
+            console.log(this.prevNum);
         },
     };
     $(function(){
